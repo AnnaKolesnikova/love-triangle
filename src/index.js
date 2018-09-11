03a8c9ffeadfa590c9c4d22d06c8703b7b304af2
Anna Kolesnikova <ani.kolesnikova@gmail.com>
2018-09-11 11:01:33 +0300

    love-triangle

--- src/index.js
+++ src/index.js
@@ -3,6 +3,30 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  array = preferences;
+  foundedTriangles = [];
 
+  for (var i = 0; i < array.length; i++) {
+    var element = array[i];
+
+    indexOfLover2 = element - 1;
+    lover2 = array[indexOfLover2];
+
+    indexOfLover3 = lover2 - 1;
+    lover3 = array[indexOfLover3];
+    if (
+      lover3 - 1 == i &&
+      element != lover2 &&
+      lover2 != lover3 &&
+      lover3 != element
+    ) {
+      triangle = {
+        lover1: element,
+        lover2: lover2,
+        lover3: lover3
+      };
+      foundedTriangles.push(triangle);
+    }
+  }
+  return foundedTriangles.length / 3;
 };
